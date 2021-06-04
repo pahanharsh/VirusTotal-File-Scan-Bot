0a22914fafaf546c732a13427a422d469ced3350
pahanharsh <82528202+pahanharsh@users.noreply.github.com>
2021-06-04 15:59:33 +0530

    Update __init__.py

--- VirusTotalAVBot/__init__.py
+++ VirusTotalAVBot/__init__.py
@@ -1,11 +1,11 @@
 import os
 
 BOT_TOKEN = os.environ.get('1852012748:AAGx2k8JpFTOk0YAypxxmEPbXKmRODZtTZI')
-API_ID = int(os.environ.get('4763422'))
+API_ID = int(os.environ.get('4763422')
 API_HASH = os.environ.get('87ec2c03054386ef4aea87fb09050a1b')
 BOT_USERNAME = os.environ.get('thevirustotal_bot')
 BOT_OWNER = os.environ.get('Humangasor')
 PLUG_IN = dict(root="VirusTotalAVBot.modules")
 VT_API = os.environ.get('41d19c915cbd5f472a1dc27938462315a70237e7c903ac6d2f7f6e8728e945a4')
 GROUP_INFO_MSGS = os.environ.get('GROUP_INFO_MSGS')
-MAX_FILE_SIZE = int(os.environ.get('110'))
+MAX_FILE_SIZE = int(os.environ.get('110')
